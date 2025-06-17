0ba74dab2b0deebeea68756ec9c9988ac04e6f8e
MartinBock1 <martinbock@arcor.de>
2025-06-17 09:50:06 +0200

    feat: created signals.py, added CoderrAppConfig in apps.py & __init__.py

---
+++ coderr_app/__init__.py
@@ -0,0 +1,3 @@
+
+
+default_app_config = 'coderr_app.apps.CoderrAppConfig'
--- coderr_app/apps.py
+++ coderr_app/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CoderrAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coderr_app'
+
+class CoderrAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'coderr_app'
+
+    def ready(self):
+        import coderr_app.signals
