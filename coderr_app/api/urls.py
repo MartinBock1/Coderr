8a81acd75204c9f9604106b7d1b584ae90113340
MartinBock1 <martinbock@arcor.de>
2025-06-17 10:51:00 +0200

    refactor: views.py added permissions, deleted comment in urls.py, renamed tests.py

--- coderr_app/api/urls.py
+++ coderr_app/api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import ProfileDetailView
 
 urlpatterns = [
-    # Der Name 'pk' im Pfad muss mit `lookup_url_kwarg` in der View übereinstimmen.
     path('profile/<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
 ]
--- coderr_app/api/views.py
+++ coderr_app/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics, permissions
+
 from coderr_app.models import Profile
 from .serializers import ProfileSerializer
+from .permissions import IsOwnerOrReadOnly 
 
 class ProfileDetailView(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.select_related('user').all()
     serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     lookup_field = 'user__pk' 
     lookup_url_kwarg = 'pk'
